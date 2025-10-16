401b5185318dd88dc3e94ed3ce2b7d67ff3c817a
Shreyash-chavare <shreyashchavare04@gmail.com>
2025-10-17 00:36:20 +0530

    added workflow

--- server/server.js
+++ server/server.js
@@ -1,19 +1,19 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const cors = require('cors');
-const dotenv = require('dotenv');
-const taskRoutes = require('./routes/tasks');
+import express, { json, urlencoded } from 'express';
+import { connect, connection } from 'mongoose';
+import cors from 'cors';
+import { config } from 'dotenv';
+import taskRoutes from './routes/tasks';
 
 // Load environment variables
-dotenv.config();
+config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(cors());
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(json());
+app.use(urlencoded({ extended: true }));
 
 // Routes
 app.use('/api/tasks', taskRoutes);
@@ -37,8 +37,7 @@ app.use('*', (req, res) => {
 let foo
 
 // Database connection
-mongoose
-  .connect(process.env.MONGO_URI, {
+connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
@@ -56,6 +55,6 @@ mongoose
 // Graceful shutdown
 process.on('SIGINT', async () => {
   console.log('dev is again changing- Shutting down gracefully...');
-  await mongoose.connection.close();
+  await connection.close();
   process.exit(0);
 });
